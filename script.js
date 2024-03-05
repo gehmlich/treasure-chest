0f511f3306c41d82a2f0d1aae53336f9e8acd46c
Josua Gehmlich <josua.gehmlich@baywa.de>
2024-03-05 15:36:34 +0100

    Refactor comment, Function location and add confetti effect when the chest is opened

--- script.js
+++ script.js
@@ -22,20 +22,6 @@ function closePopup() {
   document.getElementById("codePopup").style.display = "none";
 }
 
-// Get the input field
-var input = document.getElementById("codeInput");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function (event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("submitBtn").click();
-  }
-});
-
 // Function to add an image on top of the application
 function addTopImage() {
   // Create the image container
@@ -67,10 +53,41 @@ function addTopImage() {
     centeredText.style.animation = "fadeInText 1s ease-in";
     centeredText.style.opacity = 1; // Set opacity to 1 to start the fadeInText animation
     // Trigger confetti effect
-    confetti({
-      particleCount: 600,
-      spread: 100,
-      origin: { y: 0.7, x: 0.63 },
-    });
+    popConfetti();
   });
 }
+
+// Function to pop the Confetti effect
+function popConfetti() {
+  confetti({
+    particleCount: 600,
+    spread: 100,
+    origin: { y: 0.7, x: 0.63 },
+  });
+}
+
+// Get the input field
+var input = document.getElementById("codeInput");
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function (event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("submitBtn").click();
+  }
+});
+
+// Get toggle-treasure checkbox
+var toggleTreasure = document.getElementById("toggle-treasure");
+// Add event listener to toggle-treasure checkbox
+toggleTreasure.addEventListener("change", function () {
+  if (toggleTreasure.checked) {
+    // If the checkbox is checked, trigger the confetti effect
+    popConfetti();
+  } else {
+    // If the checkbox is unchecked, stop the confetti effect
+    confetti.stop();
+  }
+});
