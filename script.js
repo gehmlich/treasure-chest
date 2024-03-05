7f8926e2544041488d5e0184289d68b1fa6e1c4c
Josua Gehmlich <josua.gehmlich@baywa.de>
2024-03-05 14:59:58 +0100

    Add map with text and a confetti animation

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function checkCode() {
 
   if (enteredCode === correctCode) {
     document.getElementById("toggle-treasure").checked = true;
+    addTopImage();
     closePopup();
   } else {
     alert("Der Code ist falsch. Bitte versuche es erneut.");
@@ -34,3 +35,42 @@ input.addEventListener("keyup", function (event) {
     document.getElementById("submitBtn").click();
   }
 });
+
+// Function to add an image on top of the application
+function addTopImage() {
+  // Create the image container
+  var imageContainer = document.createElement("div");
+  imageContainer.classList.add("image-container");
+
+  // Create the image element
+  var topImage = document.createElement("img");
+  topImage.src = "map.png";
+  topImage.classList.add("top-image");
+  topImage.style.animation = "flyIn 1s ease-in"; // Add fly-in animation
+
+  // Create the centered text element
+  var centeredText = document.createElement("div");
+  centeredText.classList.add("centered-text");
+  centeredText.textContent =
+    "Herzlichen Glückwunsch! Du hast alle Rätsel gelöst.";
+
+  // Append the image and text elements to the container
+  imageContainer.appendChild(topImage);
+  imageContainer.appendChild(centeredText);
+
+  // Append the container to the document body
+  document.body.appendChild(imageContainer);
+
+  // Wait for the flyIn animation to finish
+  topImage.addEventListener("animationend", function () {
+    // Add the fadeInText animation after flyIn animation is done
+    centeredText.style.animation = "fadeInText 1s ease-in";
+    centeredText.style.opacity = 1; // Set opacity to 1 to start the fadeInText animation
+    // Trigger confetti effect
+    confetti({
+      particleCount: 600,
+      spread: 100,
+      origin: { y: 0.7, x: 0.63 },
+    });
+  });
+}
